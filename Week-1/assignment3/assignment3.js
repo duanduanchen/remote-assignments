00a25006618e4400c6bc752c6c96bdaa84a5e37e
陳端博 <chenduanbo@chenduanbodeMacBook-Air.local>
2023-04-04 18:20:39 +0800

    update week-1 folder

---
+++ Week-1/assignment3/assignment3.js
@@ -0,0 +1,38 @@
+function countAandB(input) {
+    let array = input;
+    let count = 0;
+    
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 'a')
+          count++; 
+        else if (array[i] === 'b') 
+          count++;
+      }
+    
+    return count
+}
+
+function toNumber(input) {
+  let array = input;
+  
+  for (let i = 0; i < array.length; i++) {
+      if (array[i] === 'a')
+        array[i] === 1; 
+      else if (array[i] === 'b') 
+        array[i] === 2; 
+      else if (array[i] === 'c') 
+        array[i] === 3;   
+      else if (array[i] === 'd') 
+        array[i] === 4; 
+      else if (array[i] === 'e') 
+        array[i] === 5; 
+    }
+  
+  return array
+}
+
+let input = ['a','b','c']
+console.log(countAandB(input))
+console.log(toNumber(input))
+
+// hello
