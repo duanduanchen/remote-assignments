109656971d0145b64f849668cbb3ccecf60b521b
陳端博 <chenduanbo@chenduanbodeMacBook-Air.local>
2023-04-05 12:11:37 +0800

    update as3

--- Week-1/Assignment-3/assignment3.js
+++ Week-1/Assignment-3/assignment3.js
@@ -13,26 +13,28 @@ function countAandB(input) {
 }
 
 function toNumber(input) {
-  let array = input;
-  
-  for (let i = 0; i < array.length; i++) {
-      if (array[i] === 'a')
-        array[i] === 1; 
-      else if (array[i] === 'b') 
-        array[i] === 2; 
-      else if (array[i] === 'c') 
-        array[i] === 3;   
-      else if (array[i] === 'd') 
-        array[i] === 4; 
-      else if (array[i] === 'e') 
-        array[i] === 5; 
-    }
-  
-  return array
+    let array = input;
+    
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 'a')
+          array[i] = 1; 
+        else if (array[i] === 'b') 
+          array[i] = 2; 
+        else if (array[i] === 'c') 
+          array[i] = 3;   
+        else if (array[i] === 'd') 
+          array[i] = 4; 
+        else if (array[i] === 'e') 
+          array[i] = 5; 
+      }
+    
+    return array
 }
 
-let input = ['a','b','c']
-console.log(countAandB(input))
-console.log(toNumber(input))
+let input1 = ['a','b','c','a','c','a','c']
+console.log(countAandB(input1))
+console.log(toNumber(input1))
 
-// hello
+let input2 = ['e','d','c','d','e']
+console.log(countAandB(input2))
+console.log(toNumber(input2))
